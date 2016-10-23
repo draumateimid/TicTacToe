d39c78fcd902ba936b36b413dc462ac35f73bae7
AgustOrnEinarsson <gustieinars@gmail.com>
2016-10-23 02:50:36 -0700

    Implemented gradle javadoc which creates api documentation for the project

--- src/main/java/com/draumateymid/HelloWorld/HelloWorld.java
+++ src/main/java/com/draumateymid/HelloWorld/HelloWorld.java
@@ -1,5 +1,13 @@
 package com.draumateymid.HelloWorld;
 
+
+/**
+ * this class ...
+ * @author agust.einarsson
+ * @param text the input string
+ *
+ */
+
 public class HelloWorld {
      public static void main(String[] args){
         World world = new World();
