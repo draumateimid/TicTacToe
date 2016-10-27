a11edfa1f78a554e1b1495802bbee49173c2d622
Guðný Kjartansdóttir <Gun Kjartansdttir>
2016-10-27 18:58:42 +0000

    Test folder, class GameStatus

---
+++ src/main/java/com/draumateymid/tictactoe/GameStatus.java
@@ -0,0 +1,8 @@
+package com.draumateymid.tictactoe;
+
+public class GameStatus{
+	public static boolean isEmpty(char[] board, int input){
+		return true; 
+	}
+
+}
---
+++ src/test/java/com/draumateymid/tictactoe/GameStatusTest.java
@@ -0,0 +1,13 @@
+package com.draumateymid.tictactoe;
+
+import static org.junit.Assert.assertEquals; 
+import org.junit.Test; 
+
+public class GameStatusTest{
+	
+	@Test
+	public void testIsEmpty(){
+		char[] board = {'E', 'X', 'O', 'E', 'E', 'O', 'E', 'X', 'X'};
+		assertEquals(false, GameStatus.isEmpty(board, 1)); 
+	}
+}
