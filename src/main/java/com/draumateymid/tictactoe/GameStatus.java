5c85a48157e5f412b30f92b06f4923363f78575b
Guðný Kjartansdóttir <Gun Kjartansdttir>
2016-10-28 18:34:36 +0000

    Changes made: ' ' for 'E'

--- src/main/java/com/draumateymid/tictactoe/GameStatus.java
+++ src/main/java/com/draumateymid/tictactoe/GameStatus.java
@@ -2,7 +2,7 @@ package com.draumateymid.tictactoe;
 
 public class GameStatus{
 	public static boolean isEmpty(char[] board, int input){
-		if(board[input-1] == 'E')
+		if(board[input-1] == ' ')
 				return true; 
 		else
 			return false; 	
@@ -10,7 +10,7 @@ public class GameStatus{
 
 	public static boolean checkWinnerHorizontal(char[] board){
 		for(int i = 0; i < 9; i += 3){
-			if(board[i] == board[i+1] && board[i+1] == board[i+2])
+			if(board[i] == board[i+1] && board[i+1] == board[i+2] && board[i] != ' ')
 					return true;
 		}
 		return false; 
@@ -18,16 +18,16 @@ public class GameStatus{
 
 	public static boolean checkWinnerVertical(char[] board){
 		for(int i = 0; i < 3; i++){
-			if(board[i] == board[i+3] && board[i+3] == board[i+6])
+			if(board[i] == board[i+3] && board[i+3] == board[i+6] && board[i] != ' ')
 				return true; 
 		}
 		return false; 
 	}
 
 	public static boolean checkWinnerDiagonal(char[] board){
-		if(board[0] == board[4] && board[4] == board[8])
+		if(board[0] == board[4] && board[4] == board[8] && board[0] != ' ')
 			return true; 
-		if(board[2] == board[4] && board[4] == board[6])
+		if(board[2] == board[4] && board[4] == board[6] && board[0] != ' ')
 			return true; 
 		return false; 
 	}
--- src/test/java/com/draumateymid/tictactoe/GameStatusTest.java
+++ src/test/java/com/draumateymid/tictactoe/GameStatusTest.java
@@ -7,49 +7,49 @@ public class GameStatusTest{
 	
 	@Test
 	public void testIsEmpty(){
-		char[] board = {'E', 'X', 'O', 'E', 'E', 'O', 'E', 'X', 'X'};
+		char[] board = {' ', 'X', 'O', ' ', ' ', 'O', ' ', 'X', 'X'};
 		assertEquals(true, GameStatus.isEmpty(board, 4)); 
 	}
 
 	@Test
 	public void testIfWinnerHorizontal(){
-		char[] board = {'X', 'X', 'X', 'E', 'O', 'O', 'E', 'E', 'E'};
+		char[] board = {'X', 'X', 'X', ' ', 'O', 'O', ' ', ' ', ' '};
 		assertEquals(true, GameStatus.checkWinnerHorizontal(board)); 
 	}
 
 	@Test
 	public void testIfWinnerVertical(){
-		char[] board = {'E', 'O', 'X', 'E', 'O', 'X', 'E', 'E', 'X'};
+		char[] board = {' ', 'O', 'X', ' ', 'O', 'X', ' ', ' ', 'X'};
 		assertEquals(true, GameStatus.checkWinnerVertical(board)); 
 	}
 
 	@Test
 	public void testIfWinnerDiagonal(){
-		char[] board = {'O', 'X', 'X', 'E', 'O', 'E', 'X', 'X', 'O'};
+		char[] board = {'O', 'X', 'X', ' ', 'O', ' ', 'X', 'X', 'O'};
 		assertEquals(true, GameStatus.checkWinnerDiagonal(board)); 
 	}
 
 	@Test
 	public void testIfWinner(){
-		char[] board = {'O', 'O', 'X', 'E', 'O', 'X', 'E', 'X', 'X'};
+		char[] board = {'O', 'O', 'X', ' ', 'O', 'X', ' ', 'X', 'X'};
 		assertEquals(true, GameStatus.checkWinner(board)); 
 	}
 
 	@Test
 	public void testIfDraw(){
-		char[] board = {'X', 'E', 'X', 'O', 'O', 'X', 'X', 'O', 'X'};
+		char[] board = {'X', ' ', 'X', 'O', 'O', 'X', 'X', 'O', 'X'};
 		assertEquals(true, GameStatus.draw(board)); 
 	}
 
 	@Test
 	public void testGameIsOverDraw(){
-		char[] board = {'X', 'E', 'X', 'O', 'O', 'X', 'X', 'O', 'X'};
+		char[] board = {'X', ' ', 'X', 'O', 'O', 'X', 'X', 'O', 'X'};
 		assertEquals(true, GameStatus.gameIsOver(board)); 
 	}
 
 	@Test
 	public void testGameIsOverWin(){
-		char[] board = {'X', 'X', 'X', 'O', 'O', 'E', 'E', 'E', 'E'};
+		char[] board = {'X', 'X', 'X', 'O', 'O', ' ', ' ', ' ', ' '};
 		assertEquals(true, GameStatus.gameIsOver(board)); 
 	}
 }
