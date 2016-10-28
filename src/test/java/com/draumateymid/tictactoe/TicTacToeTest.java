cb7c36dd2379219254b88da17ab7335c4b751498
AgustOrnEinarsson <gustieinars@gmail.com>
2016-10-28 12:28:01 -0700

    Main class set on hold, await compleation of GameRunner and GameStatus classes

--- src/test/java/com/draumateymid/tictactoe/TicTacToeTest.java
+++ src/test/java/com/draumateymid/tictactoe/TicTacToeTest.java
@@ -3,6 +3,19 @@ package com.draumateymid.tictactoe;
 public class TicTacToeTest{
 
 	@Test
-	public void test
+	public void test 
 
 }
+
+
+
+class StubGameRunner implements GameRunner {
+
+	public movePlayer(char in){
+	
+		return "the actuan output from movePlayer";
+	}
+
+}
+
+
