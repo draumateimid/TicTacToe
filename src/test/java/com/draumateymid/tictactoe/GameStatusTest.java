7455f5520cdfec89f51f40ee0dfa478ee083a94d
Guðný Kjartansdóttir <Gun Kjartansdttir>
2016-10-29 16:14:04 +0000

    Tests that return false added

--- src/test/java/com/draumateymid/tictactoe/GameStatusTest.java
+++ src/test/java/com/draumateymid/tictactoe/GameStatusTest.java
@@ -14,27 +14,39 @@ public class GameStatusTest{
 	}
 
 	@Test
-	public void testIfWinnerVertical(){
+	public void testWinnerVertical(){
 		char[] board = {' ', 'O', 'X', ' ', 'O', 'X', ' ', ' ', 'X'};
 		assertEquals(true, GameStatus.checkWinner(board)); 
 	}
 
 	@Test
-	public void testIfWinnerDiagonal(){
+	public void testWinnerDiagonal(){
 		char[] board = {'O', 'X', 'X', ' ', 'O', ' ', 'X', 'X', 'O'};
 		assertEquals(true, GameStatus.checkWinner(board)); 
 	}
 
 	@Test
-	public void testIfWinner(){
+	public void testWinner(){
 		char[] board = {'O', 'O', 'X', ' ', 'O', 'X', ' ', 'X', 'X'};
 		assertEquals(true, GameStatus.checkWinner(board)); 
 	}
 
 	@Test
-	public void testIfDraw(){
+	public void testNotWinner(){
+		char[] board = {'X', 'X', 'O', ' ', 'O', 'O', 'X', 'X', ' '};
+		assertEquals(false, GameStatus.checkWinner(board)); 
+	}
+
+	@Test
+	public void testDraw(){
 		char[] board = {'X', ' ', 'X', 'O', 'O', 'X', 'X', 'O', 'X'};
 		assertEquals(true, GameStatus.draw(board)); 
+	}
+
+	@Test
+	public void testNotDraw(){
+		char[] board = {'X', ' ', 'X', 'O', ' ', 'X', 'X', 'O', 'O'};
+		assertEquals(false, GameStatus.draw(board)); 
 	}
 
 	@Test
@@ -47,5 +59,11 @@ public class GameStatusTest{
 	public void testGameIsOverWin(){
 		char[] board = {'X', 'X', 'X', 'O', 'O', ' ', ' ', ' ', ' '};
 		assertEquals(true, GameStatus.gameIsOver(board)); 
+	}
+
+	@Test
+	public void testGameIsNotOver(){
+		char[] board = {'X', 'X', ' ', 'O', ' ', ' ', ' ', ' ', ' '};
+		assertEquals(false, GameStatus.gameIsOver(board)); 
 	}
 }
