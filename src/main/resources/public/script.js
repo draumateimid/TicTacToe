3d6056963a39471b4b7297f5ad5e3c6c0f7cfa56
Ágúst Örn Einarsson <agust.einarsson@marel.com>
2016-10-30 08:45:00 +0000

    Preliminar post/get in javascript

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,92 +1,109 @@
 
-var h = document.getElementById("res1");
-var j = document.getElementById("1")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction1(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+$(document).ready(function() {
+            var field1 = document.getElementById("res1");;
+            form.submit(function ( event ) {
+                $.ajax({
+                    type: form.attr('method'),
+                    url: form.attr('action'),
+                    data: '1'
+                }).done(function(movePlayer) {
+                    $('#res1').html(movePlayer).attr('class', 'alert alert-success');
+                }).fail(function() {
+                    $('#res1').html('Something went wrong...sorry try again.').attr('class', 'alert alert-danger');
+                });
+                event.preventDefault();
+            });
+        });
 
-var h = document.getElementById("res2");
-var j = document.getElementById("2")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction2(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+		
+	var h = document.getElementById("res1");
+	var j = document.getElementById("1")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction1(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res3");
-var j = document.getElementById("23")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction3(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+	var h = document.getElementById("res2");
+	var j = document.getElementById("2")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction2(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res4");
-var j = document.getElementById("4")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction4(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+	var h = document.getElementById("res3");
+	var j = document.getElementById("23")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction3(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res5");
-var j = document.getElementById("5")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction5(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+	var h = document.getElementById("res4");
+	var j = document.getElementById("4")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction4(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res6");
-var j = document.getElementById("6")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction6(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+	var h = document.getElementById("res5");
+	var j = document.getElementById("5")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction5(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res7");
-var j = document.getElementById("7")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction7(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+	var h = document.getElementById("res6");
+	var j = document.getElementById("6")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction6(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res8");
-var j = document.getElementById("8")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction8(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
-	}
+	var h = document.getElementById("res7");
+	var j = document.getElementById("7")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction7(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
 
-var h = document.getElementById("res9");
-var j = document.getElementById("9")
-h.style.display="none";
-j.style.display = "inline";
-function myFunction9(){
-	h.style.display="inline";
-	j.style.display="none";
-	return false;
+	var h = document.getElementById("res8");
+	var j = document.getElementById("8")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction8(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
+		}
+
+	var h = document.getElementById("res9");
+	var j = document.getElementById("9")
+	h.style.display="none";
+	j.style.display = "inline";
+	function myFunction9(){
+		h.style.display="inline";
+		j.style.display="none";
+		return false;
 	}
 
 			
